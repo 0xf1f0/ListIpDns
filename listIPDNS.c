d9ba4bf0b365f47b355ecf6b08f2b7826d124115
Busiedblowfish <oobey1@aol.com>
2016-11-05 07:47:06 -0400

    Validated IP and Subnet Mask

--- listIPDNS.c
+++ listIPDNS.c
@@ -1,55 +1,103 @@
-	#include <stdio.h>
-	#include <netdb.h>
-	#include <arpa/inet.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-	#define MAX 128
-	/* Function prototypes */
-	void dotted_decimal();
+#define MAX 128
 
-	int main (int argc, char *argv[])
-	{
-		if(argc != 3)
-		{
-			printf("Usage: %s Subnet_Mask Network_Addr", argv[0]);		
-			return -1;
-		}
-		
-		uint32_t net_ip;				//network/IP address in 32-bit long
-		uint32_t subnet_mask;			//subnet mask in 32-bit long
-		char *octet1;					//First octet of the subnet_mask
-		char *octet2	;				//Second octet of the subnet_mask
-		char *octet3;					//Third octet of the subnet_mask
-		char *octet4;					//Fourth octet of the subnet_mask
-		const char *valid_subnet[8];	//Array of valid subnet values
-		valid_subnet[0] = 
-		subnet_mask = inet_addr(argv[1]); //Convert dotted decimal subnet mask to binary
-		net_ip = inet_addr(argv[2]); //Convert dotted decimal network/IP addr to binary
-		
-		//Split the subnet mask into 4 octets(tokens) and compare to valid subnet values
-		octet1 = strtok(argv[1], ".");
-		octet2 = strtok(argv[1], ".");
-		octet3 = strtok(argv[1], ".");
-		octet4 = strtok(argv[1], NULL);
-		
-				
-		
-		
-		dotted_decimal(subnet_mask, net_ip);	
-		return 0;
-	}
+/* Function prototypes */
+bool isValidIP(char *);
+bool isValidMask(char *);
+void dotted_decimal();
 
-	/*Do a bitwise AND of the subnet mask and network/IP address to find the network address.
-	This may seems redundant at first, but user cant't be trusted to input a valid network address.
-	Thus, we calcualte the network address and work with the result.
-	*/
+int main (int argc, char *argv[])
+{
+    struct in_addr net_mask, net_ip;
 
-	//Convert binary IP address to dotted decimal
-	//**This will come in handy later**
-	void dotted_decimal(uint32_t subnet, uint32_t addr)
-	{
-		struct in_addr* ip;			//Pointer to the in_addr structure
-		uint32_t bin_ip;
-		bin_ip = (addr & subnet);	//Bitwise AND operation of network addr and subnet mask
-		ip = (struct in_addr *)&bin_ip;
-		printf("The network address in dotted decimal: %s\n", inet_ntoa(*ip));
-	}
+    //Check if the user entered the right number of input
+    if(argc != 3)
+    {
+        printf("Usage: %s Subnet_Mask Network_Address\n", argv[0]);
+        return -1;
+    }
+
+    //Validate the Subnet mask address
+    else if (isValidMask(argv[1]) == 0)
+    {
+        printf("Invalid Subnet Mask\n");
+        return -1;
+    }
+
+    //Validate the Network/IP address
+    else if (isValidIP(argv[2]) == 0)
+    {
+        printf("Invalid IP address\n");
+        return -1;
+    }
+
+    else if (isValidMask(argv[1]) == 0 && isValidIP(argv[2]) == 0)
+    {
+        printf("Invalid Subnet mask and IP address\n");
+        return -1;
+    }
+    else
+        printf("Valid Input\n");
+
+    return 0;
+}
+
+//Validate the network/IP address
+/*  Convert the IP address to a decimal format "ddd.ddd.ddd.ddd"
+    which checks if the "ddd" is between 0-255
+*/
+bool isValidIP(char *ipAddr)
+{
+    struct sockaddr_in sin;
+    int ans = inet_pton(AF_INET, ipAddr, &(sin.sin_addr));
+    return ans != 0; //1 = TRUE, 0 = FALSE
+}
+
+
+//Validate the subnet mask from input
+/*  Convert the subnet mask to decimal format
+    Generate possible valid subnet mask in decimal
+    Compare with user input
+    Return 1 if found
+    else return 0
+*/
+bool isValidMask(char *pt2mask)
+{
+    unsigned long net_mask, mask, subnet_mask, i;
+    unsigned int ans = 0;
+    struct in_addr netmask;
+    subnet_mask = inet_addr(pt2mask);       //Convert dotted decimal subnet mask to network byte
+    mask = inet_aton(pt2mask, &netmask);
+    net_mask = ntohl(netmask.s_addr);       //Network byte to long Host byte order
+    printf("HEX: %x\n", net_mask );
+    printf("Subnet mask in host byte/decimal: %lu\n", net_mask);
+
+    for(i = -1; i > 0; i--)
+    {
+        if(-(i & -i) == i && mask == i)
+        {
+            ans = 1;
+            break;
+        }
+    }
+    return ans;
+}
+
+
+//Convert binary IP address to dotted decimal
+//**This will come in handy later**
+void dotted_decimal(uint32_t subnet, uint32_t addr)
+{
+    struct in_addr* ip;
+    uint32_t bin_ip;
+    bin_ip = (addr & subnet);	//Bitwise AND operation of network addr and subnet mask
+    ip = (struct in_addr *)&bin_ip;
+    printf("The network address in dotted decimal: %s\n", inet_ntoa(*ip));
+}
